ca4310565e45bcecec75d2503de3bfa147d73845
caromuffins <doodles.butter33@gmail.com>
2026-06-07 19:44:44 -0400

    chore: php-fpm en background y nginx en foreground

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,9 @@ set -e
 PORT=${PORT:-8080}
 echo "Starting on port $PORT"
 sed -i "s/RAILWAY_PORT/$PORT/g" /etc/nginx/sites-available/default
-php-fpm -D
-nginx -g "daemon off;"
+# Iniciar php-fpm en background
+php-fpm &
+# Esperar que php-fpm levante
+sleep 2
+# Iniciar nginx en foreground
+exec nginx -g "daemon off;"

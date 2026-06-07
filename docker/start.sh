402d2f77de5813da6cee88ac87c6b4cdf8df51d4
caromuffins <doodles.butter33@gmail.com>
2026-06-07 19:32:46 -0400

    chore: simplificar start.sh y mejorar nginx.conf

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+set -e
 PORT=${PORT:-8080}
+echo "Starting on port $PORT"
 sed -i "s/RAILWAY_PORT/$PORT/g" /etc/nginx/sites-available/default
+nginx -t
 php-fpm -D
+sleep 1
 nginx -g "daemon off;"
